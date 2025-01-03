9f59ee3c63b4c0ac83d665e85627165a4cc0ae0f
Adarsh Patel <98180194+AdarshPatelCSIT@users.noreply.github.com>
2025-01-03 21:19:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,4 @@ function toggleMenu() {
     const isOpen = navList.style.right === '0px';
     navList.style.right = isOpen ? '-100%' : '0px';
 }
-const navList = document.querySelector('.navlist');
-const hamburger = document.querySelector('.hamburger');
-
-function toggleMenu() {
-    const isOpen = navList.style.right === '0px';
-    navList.style.right = isOpen ? '-100%' : '0px';
-}
 
